83e43bc986ec12864debe68e7f3fd083eb660a8a
ArrecheCristian <arrechecris@gmail.com>
2020-06-05 20:38:58 -0300

    Analisis de operaciones. Multiplicacion ABC funcionando

---
+++ Practicas/P04/analisis_tiempos.md
@@ -0,0 +1,35 @@
+Análisis de tiempos para ver de qué manera realizamos el ejercicio para entregar. Es decir, cuál es la forma más óptima de realizar las multiplicaciones, si mediante operaciones punto a punto o mediante operaciones colectivas.
+
+
+# Multiplicación con operaciones punto a punto
+## 1 proceso
+N = 512 --> 1.198218
+N = 1024 --> 10.018874
+N = 2048 --> 80.444299
+## 2 proceso
+N = 512 --> 0.605188
+N = 1024 --> 5.089371
+N = 2048 --> 40.569648
+## 4 proceso
+N = 512 --> 0.316190
+N = 1024 --> 2.647106
+N = 2048 --> 20.742114
+
+
+# Multiplicación con operaciones colectivas
+## 1 proceso
+N = 512 --> 1.132992
+N = 1024 --> 10.004423
+N = 2048 --> 79.444427
+## 2 proceso
+N = 512 --> 0.569572
+N = 1024 --> 4.952024
+N = 2048 --> 40.035495
+## 4 proceso
+N = 512 --> 0.303392
+N = 1024 --> 2.542188
+N = 2048 --> 20.060370
+
+
+# Conclusión
+Bueno la diferencia no es mucha, pero en todos los casos, por más mínimo que sea, conviene usar operaciones colectivas. Y por suerte coincide con lo que dijo el profe, que las operaciones colectivas eran más óptimas.
---
+++ Practicas/P04/entrega.c
@@ -0,0 +1,144 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <string.h>
+
+#define ROOT 0
+
+double dwalltime();
+
+
+int main(int argc, char* argv[]){
+    
+    int cantidadDeProcesos, id, N;
+    double *A, *B, *C, *ab, *abc;                  //matrices utilizadas
+    int filas_por_proceso;                          
+    int i, j, k;
+    double timetick;                    //medicion de tiempo
+    register double aux;                //variable temporal con directiva register
+    int check = 1;                      //verifica resultado
+    MPI_Status status;
+
+    if ((argc != 2) || ((atoi(argv[1])) <= 0)){                 //verifico parametros
+        printf("\nUsar: %s n\n  n: Dimension de la matriz \n", argv[0]);
+        exit(1);
+    }
+
+	MPI_Init(&argc, &argv);                                  //se inicializa el entorno de ejecucion MPI
+	MPI_Comm_rank(MPI_COMM_WORLD,&id);
+	MPI_Comm_size(MPI_COMM_WORLD,&cantidadDeProcesos);
+    
+    N = atoi(argv [1]);                                     //dimension de la matriz
+    filas_por_proceso = N/cantidadDeProcesos;               //cantidad de filas que tendra que procesar cada proceso
+
+    if (id == ROOT){                                           //bloque de código que solo ejecuta el proceso 0
+        A = (double*) malloc(sizeof(double)*N*N);           //cambian las alocaciones con los demas procesos
+        B = (double*) malloc(sizeof(double)*N*N);
+        C = (double*) malloc(sizeof(double)*N*N);
+        ab = (double*) malloc(sizeof(double)*N*filas_por_proceso);
+        abc = (double*) malloc(sizeof(double)*N*N);
+
+        for (i = 0; i < N; i++){                                //el proceso 0 inicializa las matrices 
+            for (j = 0; j < N; j++){
+                A[i*N + j] = 1;
+                B[i + j*N] = 1;
+                C[i + j*N] = 1;     
+            }
+        }
+        //COMIENZA LA MULTIPLICACION MEDIANTE MPI
+        timetick = dwalltime();                                     //proceso 0 es el encargado de medir el tiempo
+    }
+    else{                                                       //distintas alocaciones para los demas procesos
+        A = (double*) malloc(sizeof(double)*N*filas_por_proceso);           //parte de A que me toca procesar
+        B = (double*) malloc(sizeof(double)*N*N);                           //B la recibo completa
+        C = (double*) malloc(sizeof(double)*N*N);           
+        ab = (double*) malloc(sizeof(double)*N*filas_por_proceso);         
+        abc = (double*) malloc(sizeof(double)*N*filas_por_proceso);        
+
+    }
+
+    //bloque que ejecutan todos los procesos
+    MPI_Scatter(A, filas_por_proceso*N, MPI_DOUBLE, A,                  //Se divide A en partes iguales para cada proceso (incluyendo root)
+                filas_por_proceso*N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);    //quedando almacenada nuevamente en A
+    MPI_Bcast(B, N*N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);                //B se manda completa mediante broadcast para todos los procesos
+    MPI_Bcast(C, N*N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);                //C se manda completa
+
+    for (i = 0; i < filas_por_proceso; i++){                            //cada proceso calcula su parte 
+        for (j = 0; j < N; j++){                                
+            aux = 0;
+            for (k=0; k<N; k++){
+                aux += A[i*N + k] * B[k + j*N]; 
+            }
+            ab[i*N + j] = aux;
+        }
+    }
+
+     for (i = 0; i < filas_por_proceso; i++){            
+        for (j = 0; j < N; j++){                                
+            aux = 0;
+            for (k=0; k<N; k++){
+                aux += ab[i*N + k] * C[k + j*N]; 
+            }
+            abc[i*N + j] = aux;
+        }
+    }
+
+    //Recibo el tramo procesado por cada uno de los demas procesos
+     //que sera almacenado en orden segun ID en C
+    MPI_Gather(abc, filas_por_proceso*N, MPI_DOUBLE, abc, filas_por_proceso*N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+
+
+    //Por ultimo, nuevamente solo el proceso 0 imprime el tiempo y realiza las verificaciones de resultado
+    if (id == ROOT){
+    
+        printf("Tiempo en segundos: %f\n", dwalltime() - timetick);
+
+        //solo para corroborar la correcta asignacion de C, si es una matriz chica (8, 16) la imprime
+        if ( N < 17) {             
+            printf("\n\n");
+            for (int i = 0; i < N; i++) {
+                printf("| ");
+                for (int j = 0; j < N; j++) {
+                    printf("%.f ", abc[i*N + j]);
+                }
+                printf("|\n");
+            }
+        }
+    
+        //Verifica el resultado
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                check = check && (abc[i*N + j] == N*N);
+            }
+        }   
+
+        if(check){
+            printf("Multiplicacion de matrices: Resultado correcto\n");
+        }else{
+            printf("Multiplicacion de matrices: Resultado erroneo\n");
+        }
+    }
+    
+   /* free(A);
+    free(B);
+    free(C);
+    free(ab);
+    free(abc);*/
+    
+	MPI_Finalize();
+	return(0);
+}
+
+
+
+//Para calcular tiempo
+double dwalltime(){
+    double sec;
+    struct timeval tv;
+
+    gettimeofday(&tv,NULL);
+    sec = tv.tv_sec + tv.tv_usec/1000000.0;
+    return sec;
+}
