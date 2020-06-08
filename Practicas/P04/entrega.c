f981d13925052b19475a70a32398f8e1a40c0f01
angelborini <borini451@gmail.com>
2020-06-08 18:17:44 -0300

    AVG

--- Practicas/P04/entrega.c
+++ Practicas/P04/entrega.c
@@ -15,10 +15,11 @@ double dwalltime();
 int main(int argc, char* argv[]){
     
     int cantidadDeProcesos, id, N;
-    double *A, *B, *C, *ab, *abc, *ac, *abc_sum_ac;                  //matrices utilizadas
+    double *A, *B, *C, *D, *ab, *abc, *ac, *R;                  //matrices utilizadas
     int filas_por_proceso;                          
     int i, j, k;
     double timetick;                    //medicion de tiempo
+    double avgABC, avgAC, avgABC_global, avgAC_global, avgMul;
     register double aux;                //variable temporal con directiva register
     int check = 1;                      //verifica resultado
     MPI_Status status;
@@ -39,10 +40,14 @@ int main(int argc, char* argv[]){
         A = (double*) malloc(sizeof(double)*N*N);           //cambian las alocaciones con los demas procesos
         B = (double*) malloc(sizeof(double)*N*N);
         C = (double*) malloc(sizeof(double)*N*N);
+        D = (double*) malloc(sizeof(double)*N*N);
+
         ab = (double*) malloc(sizeof(double)*N*filas_por_proceso);
         ac = (double*) malloc(sizeof(double)*N*N);
         abc = (double*) malloc(sizeof(double)*N*N);
-        abc_sum_ac = (double*) malloc(sizeof(double)*N*N);
+        R = (double*) malloc(sizeof(double)*N*N);
+   
+
 
 
         for (i = 0; i < N; i++){                                //el proceso 0 inicializa las matrices 
@@ -50,6 +55,7 @@ int main(int argc, char* argv[]){
                 A[i*N + j] = 1;
                 B[i + j*N] = 1;
                 C[i + j*N] = 1;     
+                D[i + j*N] = 1; 
             }
         }
         //COMIENZA LA MULTIPLICACION MEDIANTE MPI
@@ -59,10 +65,14 @@ int main(int argc, char* argv[]){
         A = (double*) malloc(sizeof(double)*N*filas_por_proceso);           //parte de A que me toca procesar
         B = (double*) malloc(sizeof(double)*N*N);                           //B la recibo completa
         C = (double*) malloc(sizeof(double)*N*N);           
+        D = (double*) malloc(sizeof(double)*N*N);
         ab = (double*) malloc(sizeof(double)*N*filas_por_proceso);
         ac = (double*) malloc(sizeof(double)*N*filas_por_proceso);         
         abc = (double*) malloc(sizeof(double)*N*filas_por_proceso);
-        abc_sum_ac = (double*) malloc(sizeof(double)*N*filas_por_proceso);
+        R = (double*) malloc(sizeof(double)*N*filas_por_proceso);
+        
+
+        
     }
 
     //bloque que ejecutan todos los procesos
@@ -70,6 +80,8 @@ int main(int argc, char* argv[]){
                 filas_por_proceso*N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);    //quedando almacenada nuevamente en A
     MPI_Bcast(B, N*N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);                //B se manda completa mediante broadcast para todos los procesos
     MPI_Bcast(C, N*N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);                //C se manda completa
+    MPI_Bcast(D, N*N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);  
+
 
     //Multiplicacion AB
     for (i = 0; i < filas_por_proceso; i++){                            //cada proceso calcula su parte 
@@ -104,16 +116,42 @@ int main(int argc, char* argv[]){
         }
     }
 
-    //suma ABC + AC
+    
+    // AVG
+    //promedio local a cada proceso en su parte de la matriz
+    avgABC = 0;
+    avgAC = 0;
+
+    for (i = 0; i < filas_por_proceso; i++){            //cada proceso calcula su promedio local
+        for (j = 0; j < N; j++){
+           avgABC += abc[i*N + j]; 
+           avgAC += ac[i*N + j];
+        }
+    }
+
+    avgABC = avgABC/(filas_por_proceso*N);
+    avgAC = avgAC/(filas_por_proceso*N);
+    
+
+    MPI_Allreduce(&avgABC, &avgABC_global, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(&avgAC, &avgAC_global, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+
+    avgABC_global = avgABC_global/cantidadDeProcesos;
+    avgAC_global = avgAC_global/cantidadDeProcesos;
+
+    avgMul = avgAC_global*avgABC_global; 
+
+    //suma ABC + AC + avgABC*avgAC*D
     for (i = 0; i < filas_por_proceso; i++){            
         for (j = 0; j < N; j++){                                
-            abc_sum_ac[i*N + j] = abc[i*N + j] + ac[i*N + j]; 
+            R[i*N + j] = abc[i*N + j] + ac[i*N + j]+avgMul*D[i*N+j] ; 
         }
     }
 
     //Recibo el tramo procesado por cada uno de los demas procesos
      //que sera almacenado en orden segun ID en C
-    MPI_Gather(abc_sum_ac, filas_por_proceso*N, MPI_DOUBLE, abc_sum_ac, filas_por_proceso*N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+    MPI_Gather(R, filas_por_proceso*N, MPI_DOUBLE, R, filas_por_proceso*N, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
 
 
     //Por ultimo, nuevamente solo el proceso 0 imprime el tiempo y realiza las verificaciones de resultado
@@ -127,7 +165,7 @@ int main(int argc, char* argv[]){
             for (int i = 0; i < N; i++) {
                 printf("| ");
                 for (int j = 0; j < N; j++) {
-                    printf("%.f ", abc_sum_ac[i*N + j]);
+                    printf("%.f ", R[i*N + j]);
                 }
                 printf("|\n");
             }
@@ -136,7 +174,7 @@ int main(int argc, char* argv[]){
         //Verifica el resultado
         for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
-                check = check && (abc_sum_ac[i*N + j] == (N*N)+N);
+                check = check && (R[i*N + j] == (N*N)+N+avgMul);
             }
         }   
 
@@ -150,6 +188,7 @@ int main(int argc, char* argv[]){
     free(A);
     free(B);
     free(C);
+    free(D);
     free(ab);
     free(abc);
     
